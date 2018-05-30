0e8ba41530974f9904931b0fd2fb4f3d405dbf4e
Kristine Martin <kristinenicolemartin@gmail.com>
2018-05-29 18:50:25 -0700

    updated to add npm packages and initial code

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 let figlet = require('figlet');
 let colors = require('colors');
 
+figlet('Hello World!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data.rainbow);
+});
+
